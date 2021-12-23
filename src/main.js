1d6091c2c9720dc17b29f29b360ccf174fb2e262
Stephen Evans <steve@singdaptive.com>
2021-12-23 11:58:52 -0800

    alpha first airfry output with windi

---
+++ src/main.js
@@ -0,0 +1,7 @@
+
+  import "virtual:windi.css";
+
+  document.querySelector("#app").innerHTML = `
+		<h1>Hello Airfry!</h1>
+		<a href="https://github.com/jaunt/airfry" target="_blank">Documentation</a>
+	`;
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import WindiCSS from "vite-plugin-windicss";
 // vite.config.js
 export default {
   // config options
-  root: "/src",
+  root: "./src",
   plugins: [WindiCSS()],
 };
