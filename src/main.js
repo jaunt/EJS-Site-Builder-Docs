a56ffaa8c0d879c9c183b0531e36f1dd5c79e3e5
Stephen Evans <steve@singdaptive.com>
2021-12-23 12:14:35 -0800

    Using "concurrently" to run vite and airfry in parallel. Preparing /src to be gitignored.

--- src/main.js
+++
@@ -1,7 +0,0 @@
-
-  import "virtual:windi.css";
-
-  document.querySelector("#app").innerHTML = `
-		<h1>Hello Airfry!</h1>
-		<a href="https://github.com/jaunt/airfry" target="_blank">Documentation</a>
-	`;
