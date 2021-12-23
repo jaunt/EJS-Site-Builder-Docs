a325faeedeb3a34f8db75eeb58ea7760b016f842
Stephen Evans <steve@singdaptive.com>
2021-12-23 10:14:06 -0800

    Added windi plugin. Setting up airfry.

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import "virtual:windi.css";
+
+document.querySelector("#app").innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://github.com/jaunt/airfry" target="_blank">Documentation</a>
+`;
---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import WindiCSS from "vite-plugin-windicss";
+// vite.config.js
+export default {
+  // config options
+  root: "/src",
+  plugins: [WindiCSS()],
+};
