b705530387ca44450f0f0879795dfe936ff8a235
Stephen Evans <steve@singdaptive.com>
2022-01-20 21:14:24 -0800

    Wip Styling

--- airfry/public/windi.config.js
+++ airfry/public/windi.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "windicss/helpers";
+
 export default defineConfig({
   extract: {
     // accepts globs and file paths relative to project root
     include: ["index.html", "./**/index.html"],
     exclude: ["node_modules/**/*", ".git/**/*"],
   },
+  plugins: [require("windicss/plugin/typography")],
 });
