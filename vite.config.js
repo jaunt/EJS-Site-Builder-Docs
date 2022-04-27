9e2cec195766e38d22bf32f81ba6dd4c853c6559
Stephen Evans <steve@singdaptive.com>
2022-04-26 21:35:21 -0700

    Create multipage rollupOptions from airfry generated toc.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-// vite.config.js
-export default {
+const { resolve } = require("path");
+import { defineConfig } from "vite";
+
+import toc from "./src/toc.json";
+
+const mpa = toc.reduce((acc, item) => {
+  const entry = {
+    [item.url]: resolve(__dirname, "src/" + item.url + "/index.html"),
+  };
+  return { ...acc, ...entry };
+}, {});
+
+export default defineConfig({
   // config options
   root: "./src",
   plugins: [],
   logLevel: "warn",
-};
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        ...mpa,
+      },
+    },
+  },
+});
